890983d85aa92364639366d8e1d594b37940ff97
Jonas Barsten <jonasbarsten@gmail.com>
2018-01-16 13:36:31 +0100

    Making <p>-elements a fixed height even with no content

--- imports/ui/utilities/BarstenViewer.js
+++ imports/ui/utilities/BarstenViewer.js
@@ -15,8 +15,13 @@ export default class BarstenViewer extends Component {
 
 		const content = draftToHtml(this.props.content.editorState);
 
+		console.log(content);
+
 		return (
-			<div className={this.props.className} dangerouslySetInnerHTML={{ __html: content }} />
+			<div className="barsten-viewer">
+				<div className={this.props.className} dangerouslySetInnerHTML={{ __html: content }}></div>
+				<div className="clearfix"></div>
+			</div>
 		);
 	}
 }
